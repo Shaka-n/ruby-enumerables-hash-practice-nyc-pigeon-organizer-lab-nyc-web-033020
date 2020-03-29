58815b85a6697baae4a6470f8990ebfafea181be
Garen Sahagian <g.s.sahagian@gmail.com>
2020-03-29 17:37:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 def nyc_pigeon_organizer(data)
   pp data
-  data.reduce do |memo,(key1,(key2,value))|
+  data.reduce do |memo,((key1,value),(key2,value))|
     puts key1
     puts key2
     puts value

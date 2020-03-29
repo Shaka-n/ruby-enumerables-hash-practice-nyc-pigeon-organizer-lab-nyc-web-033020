1935e76d53fb178071235a7808e528f16f49758f
Garen Sahagian <g.s.sahagian@gmail.com>
2020-03-29 19:29:16 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,14 +2,9 @@ def nyc_pigeon_organizer(data)
   pp data
   data.reduce({}) do |attribute, (attribute, options)|
     
-    value.reduce({}) do | pigeon_list, (options, name)|
+    value.reduce({}) do | name, (options, name)|
 
-      value.reduce({}) do |pigeon_list, name|
-        puts "loop 3"
-        p names
-        if pigeon_list[name] != true 
-          pigeon_list[name] = name
-        end
+      value.reduce({}) do |name|
       end
       if pigeon_list[name][options]!= true 
         pigeon_list[name][options] = options
